d178c8f26bc1724f32bfcd7e057a0bfb2b0c12be
Jming <a989051825@gmail.com>
2024-09-03 03:26:25 +0800

    refactor: UserAccount DTO 修改 新增 UUID 欄位

--- src/main/java/com/ming/shopdemo/useraccount/model/dto/UserAccountDto.java
+++ src/main/java/com/ming/shopdemo/useraccount/model/dto/UserAccountDto.java
@@ -3,13 +3,15 @@ package com.ming.shopdemo.useraccount.model.dto;
 import com.ming.shopdemo.useraccount.model.constants.Role;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public record UserAccountDto(
-    String username,
-    String email,
-    String phoneNumber,
-    Role role,
-    LocalDateTime createdDateTime,
-    LocalDateTime lastModifiedDateTime
+        UUID uuid,
+        String username,
+        String email,
+        String phoneNumber,
+        Role role,
+        LocalDateTime createdDateTime,
+        LocalDateTime lastModifiedDateTime
 ) {
 }
